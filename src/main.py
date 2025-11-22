75ed5d76afd51c6fb45c7b45f3b7204f03abc224
taham <mohamed.taha@ipsa.fr>
2025-11-22 10:42:13 +0100

    Main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from model.avion import Avion
+from model.espace_aerien import EspaceAerien
 from model.simulation import Simulation
+from ui.main_window import MainWindow
+import time
 
-simu = Simulation()
-for _ in range(3):
-    simu.espace.generer_avion_aleatoire()
+if __name__ == "__main__":
+    espace = EspaceAerien()
+    espace.ajouter_avion(Avion("AF123", 0, 0, 3000))
+    espace.ajouter_avion(Avion("BA9012", 1000, 0, 3200))
+    espace.ajouter_avion(Avion("LH321", 400, 300, 3050))
 
-simu.demarrer()
-for _ in range(5):
-    simu.mise_a_jour()
+
+    sim = Simulation()
+    sim.demarrer()
+
+    app = QApplication(sys.argv)
+    window = MainWindow(espace, simulation=sim)
+    window.show()
+    ret = app.exec()
+
+
+    sim.arreter()
+    sys.exit(ret)
+
