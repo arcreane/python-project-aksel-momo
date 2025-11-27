1fc3c3fef4e0d0aeefb90663cc22c3dddfb2309d
Aksel <aksel.ayadi@ipsa.fr>
2025-11-27 20:06:47 +0100

    Modif de avion et espace aérien

--- src/model/avion.py
+++ src/model/avion.py
@@ -3,6 +3,7 @@ import math
 
 
 class Avion:
+
     def __init__(self, identifiant: str, x: float, y: float, altitude: int):
         self.identifiant = identifiant
         self.x = x
@@ -10,23 +11,23 @@ class Avion:
         self.altitude = altitude
         self.vitesse = random.randint(400, 800)
         self.cap = random.randint(0, 359)
-        self.carburant = 100
+        self.carburant = 100.0
         self.en_vol = True
         self.alerte_collision = False
 
+        self.instruction_atterrissage = False
+        self.a_atterri = False
+
     def deplacer(self, delta_temps_heures: float):
-        if delta_temps_heures <= 0:
+        if delta_temps_heures <= 0 or not self.en_vol:
             return
 
         rad = math.radians(self.cap)
 
-
         distance_parcourue = self.vitesse * delta_temps_heures * 100
-
 
         self.x += math.cos(rad) * distance_parcourue
         self.y += math.sin(rad) * distance_parcourue
-
 
         self.carburant = max(0.0, self.carburant - (10.0 * delta_temps_heures))
 
@@ -42,4 +43,4 @@ class Avion:
         self.altitude = max(0, self.altitude - delta)
 
     def est_en_urgence(self) -> bool:
-        return self.carburant < 10 or self.alerte_collision
+        return self.carburant < 10.0 or self.alerte_collision
